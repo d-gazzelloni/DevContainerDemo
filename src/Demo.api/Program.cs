8a00d724520455a41cac05c244f7c9be93f54067
Davide Gazzelloni <dgazzelloni.ext@qubicaamf.com>
2025-02-12 13:02:55 +0100

    Add Swashbuckle.AspNetCore for API documentation and enhance weather forecast endpoint

--- src/Demo.api/Program.cs
+++ src/Demo.api/Program.cs
@@ -10,32 +10,44 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(o => { o.SwaggerEndpoint("/openapi/v1.json", "Demo API"); });
 }
 
 app.UseHttpsRedirection();
 
 var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+                {
+                        "Freezing",
+                        "Bracing",
+                        "Chilly",
+                        "Cool",
+                        "Mild",
+                        "Warm",
+                        "Balmy",
+                        "Hot",
+                        "Sweltering",
+                        "Scorching"
+                };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/weatherforecast",
+           () =>
+           {
+               var env = Environment.GetEnvironmentVariable("CUSTOM_VARIABLE");
+               if (env is null)
+                   return Results.Unauthorized();
+
+               var forecast = Enumerable.Range(1, 5)
+                                        .Select(index => new WeatherForecast(DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                                                                             Random.Shared.Next(-20, 55),
+                                                                             summaries[Random.Shared.Next(summaries.Length)]))
+                                        .ToArray();
+               return Results.Ok(forecast);
+           })
+   .WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
